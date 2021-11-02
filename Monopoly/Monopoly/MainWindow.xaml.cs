8eeff6fb1d9ef2a34b0e42b4d53ccc914bc774f2
buitongminhchau01032002 <buitongminhchau01032002@gmail.com>
2021-11-02 20:24:36 +0700

    Create Project

--- Monopoly/Monopoly/MainWindow.xaml.cs
+++ Monopoly/Monopoly/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace DESIGN
+namespace Monopoly
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -23,11 +23,6 @@ namespace DESIGN
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
