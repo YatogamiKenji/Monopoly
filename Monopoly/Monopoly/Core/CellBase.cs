3d6f213adad20ee8aa8950829869c78e539356b9
VuXuanHoangLam <blackkingofdead@gmail.com>
2021-11-10 21:34:44 +0700

    Lam's Class
    Class Base,

---
+++ Monopoly/Monopoly/Core/CellBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    abstract class CellBase
+    {
+        private string Name;
+        abstract public void Chuc_nang();
+
+    }
+}
---
+++ Monopoly/Monopoly/Core/CoHoi.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monopoly
+{
+    class CoHoi
+    {
+        private string Ten { get; set; }
+        private int Loai { get; set; }
+
+    }
+}
