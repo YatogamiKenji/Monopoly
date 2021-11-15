3d577451f5d6f8536573390051f468321cab1e9c
buitongminhchau01032002 <buitongminhchau01032002@gmail.com>
2021-11-15 19:57:55 +0700

    edit main

--- Monopoly/Monopoly/MainWindow.xaml.cs
+++ Monopoly/Monopoly/MainWindow.xaml.cs
@@ -24,52 +24,8 @@ namespace Monopoly
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window, INotifyPropertyChanged
+    public partial class MainWindow : Window
     {
-        public event PropertyChangedEventHandler PropertyChanged;
-        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-
-
-        private List<Player> _players;
-
-        public List<Player> players
-        {
-            get { return _players; }
-            set { _players = value; OnPropertyChanged(); }
-        }
-
-
-        private int _turn = 0;
-        public int turn
-        {
-            get { return _turn; }
-            set { _turn = value; OnPropertyChanged(); }
-        }
-
-        public MainWindow()
-        {
-            InitializeComponent();
-            DataContext = this;
-            turn = 1;
-            players = new List<Player>();
-            for (int i = 0; i < 4; i++)
-            {
-                List<Land> lands = new List<Land>(2) { new Land("Dat " + i, 12563, 3), new Land("Dat 2", 12345, 2)};
-                players.Add(new Player("Player" + i, 20000 + i, 0, true));
-                players[i].lands = lands;
-            }
-
-            sideBar.update(players, turn);
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            turn = (turn + 1) % 4;
-            sideBar.update(players, turn);
-        }
+        
     }
 }
