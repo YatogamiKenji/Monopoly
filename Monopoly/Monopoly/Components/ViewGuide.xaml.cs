8563636184a28eb364c53c331110c141429ecf2e
VuXuanHoangLam <blackkingofdead@gmail.com>
2021-12-24 23:32:14 +0700

    View Guide
    Thêm ViewGuide

--- Monopoly/Monopoly/Components/ViewGuide.xaml.cs
+++ Monopoly/Monopoly/Components/ViewGuide.xaml.cs
@@ -1,4 +1,7 @@
 ﻿using System.Windows.Controls;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System;
 
 namespace Monopoly.Components
 {
@@ -7,9 +10,55 @@ namespace Monopoly.Components
     /// </summary>
     public partial class ViewGuide : UserControl
     {
+        public int CurPic;
         public ViewGuide()
         {
             InitializeComponent();
+            CurPic = 1;
+        }
+        public static readonly RoutedEvent BackButtonClickEvent =
+           EventManager.RegisterRoutedEvent(nameof(OnBackButtonClick), RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(ViewStart));
+        public event RoutedEventHandler OnBackButtonClick
+        {
+            add { AddHandler(BackButtonClickEvent, value); }
+            remove { RemoveHandler(BackButtonClickEvent, value); }
+        }
+        private void Back_Click(object sender, RoutedEventArgs e)
+        {
+            Sound.BackButton();
+            RaiseEvent(new RoutedEventArgs(BackButtonClickEvent));
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            Sound.StartButton();
+            CurPic++;
+            if (CurPic == 6)
+            {
+                Next.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                Prev.Visibility = Visibility.Visible;
+                Next.Visibility = Visibility.Visible;
+            }
+            GuidePic.Source = new BitmapImage(new Uri(@"/Images/Guide/" + CurPic + ".png", UriKind.Relative));
+        }
+
+        private void Prev_Click(object sender, RoutedEventArgs e)
+        {
+            Sound.StartButton();
+            CurPic--;
+            if (CurPic == 1)
+            {
+                Prev.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                Next.Visibility = Visibility.Visible;
+                Prev.Visibility = Visibility.Visible;
+            }
+            GuidePic.Source = new BitmapImage(new Uri(@"/Images/Guide/" + CurPic + ".png", UriKind.Relative));
         }
     }
 }
